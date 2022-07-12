c5d4f3a7ddfd3b769a8d35961e5182d0983ddd02
kor9512 <def.kor9512@gmail.com>
2022-07-12 23:51:37 +0900

    Test 기본 위치

---
+++ VS/ConsoleProject/PGM/_link.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleProject
+{
+    //https://school.programmers.co.kr/learn/courses/30/lessons/
+}
