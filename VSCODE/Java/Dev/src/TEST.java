25939f707623e0725aea3b333da1a8848609b96d
Dev <def.kor9512@gmail.com>
2022-07-20 22:53:11 +0900

    VSCODE에 JAVA 세팅 완료

---
+++ VSCODE/Java/Dev/src/TEST.java
@@ -0,0 +1,7 @@
+public class TEST {
+    public static void main(String[] args) throws Exception {
+        for(int i=0; i<10 ;i++){
+            System.out.println("Hello, World!");
+        }
+    }
+}
